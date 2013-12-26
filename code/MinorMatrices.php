0ba478a1c327a869a78d660cdfe543f4b9d37a37
BumBimz <l3uml3imz@gmail.com>
2013-12-26 23:11:42 +0700

    Add SecondMinorMatrix And SecondMinorMatrixTest

--- code/MinorMatrices.php
+++ code/MinorMatrices.php
@@ -1,10 +1,20 @@
 <?php
   class MinorMatrices{
-    function minorMatrix($arraymatrix){
-      return array(
-              array(-5,1),
-              array(1,1)
-      );
+    function minorMatrix($arraymatrix,$n,$m){
+      $x=0;
+      for($i=0;$i<sizeof($arraymatrix);$i++){
+        if($i!=$n){
+          $y=0;
+          for($j=0;$j<sizeof($arraymatrix);$j++){
+            if($j!=$m){
+              $newArray[$x][$y]=$arraymatrix[$i][$j];
+              $y++;
+            }
+          }
+          $x++;
+        }
+      }
+      return $newArray;
     }
   }
 ?>
--- test/MinorMatricesTest.php
+++ test/MinorMatricesTest.php
@@ -1,6 +1,6 @@
 <?php
   class MinorMatricesTest extends PHPUnit_Framework_TestCase{
-    function testMinorMatricesRoundOneWhenAddSqureMatrixThenReturn2x2SquaresMatrix(){
+    function testMinorMatricesFirstRoundWhenAddSqureMatrixThenReturn2x2SquaresMatrix(){
       $arraymatrix = array(
                       array(3,2,1),
                       array(5,-5,1),
@@ -11,7 +11,22 @@
                     array(1,1)
                   );
       $minorMatrices = new MinorMatrices();
-      $actual = $minorMatrices->minorMatrix($arraymatrix);
+      $actual = $minorMatrices->minorMatrix($arraymatrix,0,0);
+      $this->assertEquals($expected,$actual);
+    }
+    
+    function testMinorMatricesSecondRoundWhenAddSqureMatrixThenReturn2x2SquaresMatrix(){
+      $arraymatrix = array(
+                      array(3,2,1),
+                      array(5,-5,1),
+                      array(-1,1,1),
+                    );
+      $expected = array(
+                    array(5,1),
+                    array(-1,1)
+                  );
+      $minorMatrices = new MinorMatrices();
+      $actual = $minorMatrices->minorMatrix($arraymatrix,0,1);
       $this->assertEquals($expected,$actual);
     }
   } 
