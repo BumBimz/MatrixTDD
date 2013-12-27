7b55302afd5eff14a899947cb5f2d347ffc31a3a
BumBimz <l3uml3imz@gmail.com>
2013-12-27 20:31:21 +0700

    Add New Test 'InverseMatrix'

--- test/InverseMatrixTest.php
+++ test/InverseMatrixTest.php
@@ -1,6 +1,6 @@
 <?php
   class InverseMatrixTest extends PHPUnit_Framework_TestCase{
-    function testInverseMatrixGivenSquareMatrixWhenInverseMatrixThenReturnInverseMatrix(){
+    function testInverseMatrixGivenFirstMatrixWhenInverseMatrixThenReturnInverseMatrix(){
       $inverse = new InverseMatrix();
       $arrayMatrix = array(
                       array(3,5,-1),
@@ -15,5 +15,22 @@
       $actual = $inverse->inverse($arrayMatrix);
       $this->assertEquals($expected,$actual);
     }
+
+    function testInverseMatrixGivenSecondMatrixWhenInverseMatrixThenReturnInverseMatrix(){
+      $inverse = new InverseMatrix();
+      $arrayMatrix = array(
+                      array(1,1,0),
+                      array(0,1,1),
+                      array(1,0,1)
+                    );
+      $expected = array(
+                    array(1,-1,1),
+                    array(1,1,-1),
+                    array(-1,1,1)
+                  );
+      $actual = $inverse->inverse($arrayMatrix);
+      $this->assertEquals($expected,$actual);
+    }
+
   }
 ?>
