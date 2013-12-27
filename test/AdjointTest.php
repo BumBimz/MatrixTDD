8e0281297f6c5ef31ea0df18a24c46c61361f52a
BumBimz <l3uml3imz@gmail.com>
2013-12-27 17:37:33 +0700

    Add Test And Code 'First Step'

--- test/AdjointTest.php
+++ test/AdjointTest.php
@@ -171,5 +171,20 @@
       $this->assertEquals($expected,$actual);
     }
 
+    function testAdjointGivenFirstMatrixWhenFirstStepThenReturnExpectedMatrix(){
+      $arrayMatrix = array(
+                      array(3,5,-1),
+                      array(2,-5,1),
+                      array(1,1,1)
+                     );
+      $expected = array(
+                    array(-6,6,0),
+                    array(1,4,5),
+                    array(7,-2,-25)
+                  );
+      $actual = $this->adjoint->firstStep($arrayMatrix);
+      $this->assertEquals($expected,$actual);
+
+}
   }
 ?>
