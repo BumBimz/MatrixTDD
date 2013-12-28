c7b1f1854ac52bb96cb97e828f6a8adbc2824234
BumBimz <l3uml3imz@gmail.com>
2013-12-28 20:03:22 +0700

    Edit Test Inverse Matrix

--- code/InverseMatrix.php
+++ code/InverseMatrix.php
@@ -1,11 +1,11 @@
 <?php
   class InverseMatrix{
-    function inverse($arrayMatrix){
+    function divide($arrayMatrix){
       return array(
-                    array(0.2,0.2,0),
-                    array(1/30,-2/15,1/6),
-                    array(-7/30,-1/15,5/6)
-                  ); 
+              array(6/30,6/30,0),
+              array(1/30,-4/30,5/30),
+              array(-7/30,-2/30,25/30)
+             );
     } 
   }
 ?>
--- test/InverseMatrixTest.php
+++ test/InverseMatrixTest.php
@@ -1,36 +1,37 @@
 <?php
   class InverseMatrixTest extends PHPUnit_Framework_TestCase{
     function testInverseMatrixGivenFirstMatrixWhenInverseMatrixThenReturnInverseMatrix(){
+      $expected = array(
+                    array(6/30,6/30,0),
+                    array(1/30,-4/30,5/30),
+                    array(-7/30,-2/30,25/30)
+                  );
+
+      $mockDeterminant = $this->getMock('Determinant',array('calculatorDeterminant'));
+      $mockDeterminant->expects($this->once())
+        ->method('calculatorDeterminant')
+        ->will($this->returnValue('-30'));
+
+      $expectedResultFromAdjoint=array(
+        array(-6,-6,0),
+        array(-1,4,-5),
+        array(7,2,-25)
+        );
+      $mockAdjoint = $this->getMock('Adjoint',array('adjointMatrix'));
+      $mockAdjoint->expects($this->once())
+        ->method('adjointMatrix')
+        ->will($this->returnValue($expectedResultFromAdjoint));
+      
       $inverse = new InverseMatrix();
       $arrayMatrix = array(
                       array(3,5,-1),
                       array(2,-5,1),
                       array(1,1,1)
                     );
-      $expected = array(
-                    array(6/30,6/30,0),
-                    array(1/30,-4/30,5/30),
-                    array(-7/30,-2/30,25/30)
-                  );
-      $actual = $inverse->inverse($arrayMatrix);
+      $resultDeterminant=$mockDeterminant->calculatorDeterminant($arrayMatrix);
+      $resultAdjoint=$mockAdjoint->adjointMatrix($arrayMatrix);
+      $actual = $inverse->divide($resultDeterminant,$resultAdjoint);
       $this->assertEquals($expected,$actual);
     }
-
-    function testInverseMatrixGivenSecondMatrixWhenInverseMatrixThenReturnInverseMatrix(){
-      $inverse = new InverseMatrix();
-      $arrayMatrix = array(
-                      array(1,1,0),
-                      array(0,1,1),
-                      array(1,0,1)
-                    );
-      $expected = array(
-                    array(0.5,-0.5,0.5),
-                    array(0.5,0.5,-0.5),
-                    array(-0.5,0.5,0.5)
-                  );
-      $actual = $inverse->inverse($arrayMatrix);
-      $this->assertEquals($expected,$actual);
-    }
-
   }
 ?>
