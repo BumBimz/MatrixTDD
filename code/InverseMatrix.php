e152fc3908502a8ce298b054c8fe54dd964ab6a4
BumBimz <l3uml3imz@gmail.com>
2013-12-30 01:15:48 +0700

    Refactor Inverse Code

--- code/InverseMatrix.php
+++ code/InverseMatrix.php
@@ -15,9 +15,9 @@ class InverseMatrix{
       $resultDeterminant = $this->determinant->calculatorDeterminant($arrayMatrix);
       $resultAdjoint = $this->adjoint->adjointMatrix($arrayMatrix);
       $sizeOfMatrix = sizeof($resultAdjoint);
-      for($i=0;$i<$sizeOfMatrix;$i++){
-        for($j=0;$j<$sizeOfMatrix;$j++){
-          $resultInverseMatrix[$i][$j]=$resultAdjoint[$i][$j]/$resultDeterminant;
+      for($row=0;$row<$sizeOfMatrix;$row++){
+        for($column=0;$column<$sizeOfMatrix;$column++){
+          $resultInverseMatrix[$row][$column]=$resultAdjoint[$row][$column]/$resultDeterminant;
         }
       }
       return $resultInverseMatrix;
